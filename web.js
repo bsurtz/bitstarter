1fcae3e8004d65d37ce16ffbdcc841570a62454c
bsurtz@gmail.com <bsurtz@gmail.com>
2013-07-31 02:09:02 +0000

    hw part 1

--- web.js
+++ web.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var content = fs.readFileSync('index.html');
+  var content = fs.readFileSync('index.html').toString();
   response.send(content);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
